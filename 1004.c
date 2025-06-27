f802da7f5741c9dc0157fa057143d92d002b0614
Helomaylima <helomaylima@gmail.com>
2025-06-27 14:37:14 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int X, Y, PROD;
+    scanf("%d", &X);
+    scanf("%d", &Y);
+    
+    PROD = X * Y;
+    printf("PROD = %d\n", PROD);
+ 
+    return 0;
+}

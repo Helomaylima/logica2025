6b87bab518edf0cc3f597808a9e179bff20cf8c8
Helomaylima <helomaylima@gmail.com>
2025-06-27 14:49:28 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, C, D, X;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    
+    X = (A * B) - (C * D);
+    
+    printf("DIFERENCA = %d\n", X);
+    
+    return 0;
+}
